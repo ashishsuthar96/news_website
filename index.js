fd827cd6fb3e4b8d603a16fc9ac3741041b722bd
ashishsuthar96 <80204112+ashishsuthar96@users.noreply.github.com>
2024-08-21 12:15:44 +0000

    api test

---
+++ index.js
@@ -0,0 +1,19 @@
+const API_KEY = "pub_514115d4ccc2db06e79944a4d383758334fdc";
+const PATH = "https://newsdata.io/api/1/latest?apikey=";
+
+window.addEventListener('load', () => fetchNews("india") );
+
+async function fetchNews(query){
+const res= await fetch(`${PATH}${API_KEY}&q=${query}`);
+const data = await res.json();
+
+console.log(data);
+}
+
+
+
+// https://newsdata.io/api/1/latest?apikey=pub_514115d4ccc2db06e79944a4d383758334fdc&q=pizza
+// https://newsdata.io/api/1/archive?apikey=pub_514115d4ccc2db06e79944a4d383758334fdc&q=example&language=en&from_date=2023-01-19&to_date=2023-01-25
+// https://newsdata.io/api/1/latest?apikey=pub_514115d4ccc2db06e79944a4d383758334fdc&q=YOUR-QUERY&page=XXXPPPXXXXXXXXXX
+
+// https://newsdata.io/api/1/latest?apikey=pub_514115d4ccc2db06e79944a4d383758334fdc
